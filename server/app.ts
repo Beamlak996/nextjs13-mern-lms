5e2ac02ee44695bbf05bed1ec10e835fb3bb751c
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-12-05 03:21:26 -0900

    DB, Redis and cloudinary complete

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,27 @@
-import express from "express"
+import express, { NextFunction, Request, Response } from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+require("dotenv").config()
+
 export const app = express()
+
+app.use(express.json({limit: "50mb"}))
+app.use(cookieParser())
+app.use(cors({
+    origin: process.env.ORIGIN
+}))
+
+app.get("/test", (req: Request, res: Response, next: NextFunction)=> {
+    res.status(200).json({
+        success: true,
+        message: "API is working"
+    })
+})
+
+// unknown routes
+app.all("*", (req: Request, res: Response, next: NextFunction)=> {
+    const error = new Error(`Route ${req.originalUrl} not found`) as any
+    error.statusCode = 404
+    next(error)
+})
