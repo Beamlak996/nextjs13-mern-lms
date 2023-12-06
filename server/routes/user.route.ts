f59c44f4503728fa7e5fc2ae0d25bfcb45df7abd
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-12-05 19:38:25 -0900

    Login and Logout Complete

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,12 +1,23 @@
 import express from "express";
-import { activateUser, registrationUesr } from "../controllers/user.controller";
+import {
+  activateUser,
+  loginUser,
+  logoutUser,
+  registrationUesr,
+} from "../controllers/user.controller";
 
-const userRouter = express.Router()
+const userRouter = express.Router();
 
-//register route 
-userRouter.post("/registration", registrationUesr)
+//register route
+userRouter.post("/registration", registrationUesr);
 
 // activate user
-userRouter.post("/activate-user", activateUser)
+userRouter.post("/activate-user", activateUser);
 
-export default userRouter
+// login user
+userRouter.post("/login", loginUser);
+
+// logout user
+userRouter.get("/logout", logoutUser);
+
+export default userRouter;
