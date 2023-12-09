2f30276c8444a376288586636fda096b134ed51f
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-12-08 22:14:52 -0900

    Edit Course Complete

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -3,30 +3,65 @@ import { CatchAsyncError } from "../middleware/catchAsyncError";
 import cloudinary from "cloudinary";
 import ErrorHandler from "../utils/ErrorHandler";
 import { createCourse } from "../services/course.service";
-
+import CourseModel from "../models/course.model";
 
 // upload course
 export const uploadCourse = CatchAsyncError(
-    async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const data = req.body
-            const thumbnail = data.thumbnail;
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+      const thumbnail = data.thumbnail;
 
-            if(thumbnail) {
-                const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
-                    folder: "courses",
-                })
+      if (thumbnail) {
+        const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+          folder: "courses",
+        });
 
-                data.thumbnail = {
-                    public_id: myCloud.public_id,
-                    url: myCloud.secure_url
-                }
-            }
+        data.thumbnail = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
 
-            createCourse(data, res, next)
-        } catch (error: any) {
-            return next(new ErrorHandler(error.message, 400))
-        }
+      createCourse(data, res, next);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
     }
-)
+  }
+);
 
+// edit course
+export const editCourse = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+      const thumbnail = data.thumbnail;
+
+      // Edit thumbnail
+      if (thumbnail) {
+        // destory the previous thumbnail
+        await cloudinary.v2.uploader.destroy(thumbnail?.public_id);
+
+        // add a new one
+        const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+          folder: "courses",
+        });
+      }
+
+      const courseId = req.params.id;
+
+      const course = await CourseModel.findByIdAndUpdate(
+        courseId,
+        { $set: data },
+        { new: true }
+      );
+
+      res.status(201).json({
+        success: true,
+        course,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,10 +1,12 @@
 import express from "express"
-import { uploadCourse } from "../controllers/course.controller"
+import { editCourse, uploadCourse } from "../controllers/course.controller"
 import { authorizeRoles, isAuthenticated } from "../middleware/auth"
 
 const courseRouter = express.Router()
 
 
 courseRouter.post("/create-course", isAuthenticated, authorizeRoles("admin"), uploadCourse)
+
+courseRouter.put("/edit-course/:id", isAuthenticated, authorizeRoles("admin"), editCourse)
 
 export default courseRouter
