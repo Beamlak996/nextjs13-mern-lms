bf21b193ea746ef84259d15b27434a183c0ffe08
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-12-09 23:44:47 -0900

    Create Order Complete

---
+++ server/services/order.service.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Response } from "express";
+import { CatchAsyncError } from "../middleware/catchAsyncError";
+import { OrderModel } from "../models/order.model";
+
+
+// create new order
+export const newOrder = CatchAsyncError(
+    async (data: any, res: Response) => {
+        const order = await OrderModel.create(data)
+        
+        res.status(201).json({
+            success: true,
+            order
+        })
+    }
+)
