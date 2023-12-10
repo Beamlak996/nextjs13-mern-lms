59808f57738841b9322c07e60714c7551a46e120
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-12-10 04:36:00 -0900

    Delete Notification complete

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { CatchAsyncError } from "../middleware/catchAsyncError";
 import ErrorHandler from "../utils/ErrorHandler";
 import { NotificationModel } from "../models/notification.model";
+import cron from "node-cron"
 
 
 // get all notifications
@@ -45,3 +46,9 @@ export const updateNotification = CatchAsyncError(
         }
     }
 )
+
+// delete notification
+cron.schedule("0 0 0 * * *", async() => {
+    const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000)
+    await NotificationModel.deleteMany({status: "read", createdAt: {$lt: thirtyDaysAgo}})
+})
