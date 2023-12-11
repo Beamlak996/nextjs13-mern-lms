717d2caecb62085d9e321ac02237fa549f3d1ba0
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-12-11 07:40:20 -0900

    Cache maintenace done

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -217,7 +217,7 @@ export const updateAccessToken = CatchAsyncError(
       const session = await redis.get(decoded.id);
 
       if (!session) {
-        return next(new ErrorHandler("Could not refresh token", 400));
+        return next(new ErrorHandler("Please login to access this resources", 404));
       }
 
       const user = JSON.parse(session);
@@ -242,6 +242,8 @@ export const updateAccessToken = CatchAsyncError(
 
       res.cookie("access_token", accessToken, accessTokenOptions);
       res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+
+      await redis.set(user._id, JSON.stringify(user), "EX", 604800) // 7 days
 
       res.status(200).json({
         succcess: true,
