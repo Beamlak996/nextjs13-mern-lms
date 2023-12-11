623c689390b8a1de380ff6c88aac4627b302fc2d
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-12-11 06:05:37 -0900

    Create Banner, FAQ & Categories Done

--- server/app.ts
+++ server/app.ts
@@ -1,39 +1,49 @@
-import express, { NextFunction, Request, Response } from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import { ErrorMiddleware } from "./middleware/error"
-import userRouter from "./routes/user.route"
-import courseRouter from "./routes/course.route"
-import orderRouter from "./routes/order.route"
-import notificationRouter from "./routes/notification.route"
-import analyticsRouter from "./routes/analytics.route"
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { ErrorMiddleware } from "./middleware/error";
+import userRouter from "./routes/user.route";
+import courseRouter from "./routes/course.route";
+import orderRouter from "./routes/order.route";
+import notificationRouter from "./routes/notification.route";
+import analyticsRouter from "./routes/analytics.route";
+import layoutRouter from "./routes/layout.route";
 
-require("dotenv").config()
+require("dotenv").config();
 
-export const app = express()
+export const app = express();
 
-app.use(express.json({limit: "50mb"}))
-app.use(cookieParser())
-app.use(cors({
-    origin: process.env.ORIGIN
-}))
+app.use(express.json({ limit: "50mb" }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
 
 // routes
-app.use("/api/v1", userRouter, courseRouter, orderRouter, notificationRouter, analyticsRouter);
+app.use(
+  "/api/v1",
+  userRouter,
+  courseRouter,
+  orderRouter,
+  notificationRouter,
+  analyticsRouter,
+  layoutRouter
+);
 
-
-app.get("/test", (req: Request, res: Response, next: NextFunction)=> {
-    res.status(200).json({
-        success: true,
-        message: "API is working"
-    })
-})
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working",
+  });
+});
 
 // unknown routes
-app.all("*", (req: Request, res: Response, next: NextFunction)=> {
-    const error = new Error(`Route ${req.originalUrl} not found`) as any
-    error.statusCode = 404
-    next(error)
-})
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const error = new Error(`Route ${req.originalUrl} not found`) as any;
+  error.statusCode = 404;
+  next(error);
+});
 
-app.use(ErrorMiddleware)
+app.use(ErrorMiddleware);
